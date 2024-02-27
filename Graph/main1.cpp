b6ee2aa8a7878ae6d2e3066335908e54b22eccb6
dekangLee <dakang579@gmail.com>
2024-02-27 09:20:39 +0800

    🧪 test:

---
+++ Graph/main1.cpp
@@ -0,0 +1,25 @@
+/*
+ * @Author: dekangLee dakang579@gmail.com
+ * @Date: 2024-02-26 21:20:29
+ * @LastEditors: dekangLee dakang579@gmail.com
+ * @LastEditTime: 2024-02-26 21:30:41
+ * @FilePath: \DATA_Struct\Graph\main1.cpp
+ * @Description: 
+ * 
+ * Copyright (c) 2024 by  dakang579@gmail.com, All Rights Reserved. 
+ */
+#include <iostream>
+#include "graph.hpp"
+#include "adjacencyWDigraph.hpp"
+using namespace std;
+void test()
+{
+    typedef adjacencyWDigraph<int> Graph;
+    typedef edge<int> E;
+    Graph G1;
+    
+}
+int main()
+{
+    return 0;
+}
